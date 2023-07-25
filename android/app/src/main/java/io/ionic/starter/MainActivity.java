93f36880e331f4e258117d2e4e74a087fb033b1a
Closus <claudio@manca.be>
2023-07-25 14:43:49 +0200

    [remove] mainactivity.java

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,9 +1,7 @@
 package io.ionic.starter;
 
 import com.getcapacitor.BridgeActivity;
-import com.ahm.capacitor.camera.preview.CameraPreview;
 
 
 public class MainActivity extends BridgeActivity {
-  add(CameraPreview.class);
 }
